a0b084ea673797aa25eb7f0c1b0b7be9e1289bc4
20231TIIMI0286 <leticia.opassos.500@gmail.com>
2025-10-23 08:46:34 -0300

    Pg 45

--- app/src/main/java/com/leticia/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/leticia/task/ui/auth/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.leticia.task.R
 import com.leticia.task.databinding.FragmentRegisterBinding
@@ -46,7 +48,8 @@ class RegisterFragment : Fragment() {
         val senha = binding.editTextSenha.text.toString().trim()
         if(email.isNotBlank()) {
             if (senha.isNotBlank()) {
-                findNavController().navigate(R.id.action_global_homeFragment)
+                binding.progressbar.isVisible = true
+                registerUser(email, senha)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty_register_gragment))
             }
@@ -59,16 +62,24 @@ class RegisterFragment : Fragment() {
 
         try {
             val auth = FirebaseAuth.getInstance()
-        }
+
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
 
                 }else{
-                }
+                    binding.progressbar.isVisible = false
 
-            }
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+                }
+        }catch ( e: Exception){
+            Toast.makeText(requireContext(), e.message.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
