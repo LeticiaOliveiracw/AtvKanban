c6d40d7b1a522f49aba135cfa3ec498cc2fd6ed9
20231TIIMI0286 <leticia.opassos.500@gmail.com>
2025-11-11 10:12:35 -0300

    Arrumar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
 
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx:21.0.0")
 }
--- app/src/main/java/com/leticia/task/ui/FormTaskFragment.kt
+++ app/src/main/java/com/leticia/task/ui/FormTaskFragment.kt
@@ -6,14 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.leticia.task.R
+import com.leticia.task.data.model.Status
+import com.leticia.task.data.model.Task
 import com.leticia.task.databinding.FragmentFormTaskBinding
 import com.leticia.task.util.initToolbar
 import com.leticia.task.util.showBottomSheet
+import java.lang.ref.Reference
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import initToolbar
+import showBottonSheet
 
 class FormTaskFragment : Fragment() {
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var task: Task
+
+    private var newTask: Boolean = true
+
+    private var status: Status = Status.TODO
+
+    private lateinit var reference: DatabaseReference
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +51,23 @@ class FormTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+
+        reference = Firebase.database.reference
+        auth = Firebase.auth
+
         initListener()
     }
     private fun initListener(){
         binding.buttonSave.setOnClickListener {
             valideData()
+        }
+
+        binding.radioGroup.setOnClickListener { _,id-> status =
+            when(id) {
+                R.id.rbTodo -> Status.TODO
+                R.id.rbDoing -> Status.DOING
+                else -> Status.DONE
+            }
         }
     }
 
@@ -40,9 +75,45 @@ class FormTaskFragment : Fragment() {
         val description = binding.editTextDescricao.text.toString().trim()
         if (description.isNotBlank()){
             Toast.makeText(requireContext(),"Tudo Certo!", Toast.LENGTH_SHORT).show()
+
+            binding.progressBar.isVisible = true
+
+            if (newTask) task = Task()
+            task.id = reference.database.reference.push().key ?: ""
+            task.description = description
+            task.status = status
+
+            saveTask()
+
+
         }else{
             showBottomSheet(message = getString(R.string.description_empty_form_task_fragment))
         }
+    }
+
+    private fun saveTask(){
+        reference
+            .child("task")
+            .child(auth.currentUser?.uid ?: "")
+            .child(task.id)
+            .setValue(task).addOnCompleteListener { result ->
+                if (result.isSuccessful){
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.text_save_sucess_form_task_fragment,
+                        Toast.LENGTH_SHORT).show()
+
+                    if (newTask){
+                        findNavController().popBackStack()
+
+                    }else{
+                        binding.progressBar.isVisible = false
+                    }
+                }else{
+                    binding.progressBar.isVisible = false
+                    showBottomSheet(message = getString(R.string.error_generic))
+                }
+            }
     }
 
     override fun onDestroyView() {
